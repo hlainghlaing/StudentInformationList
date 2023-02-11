b3876b3557291c3b541bb6a3a15808d773abea50
hlainghlaing <khinyadanarhlaling.29580@gmail.com>
2023-02-11 19:56:45 +0630

    Final commit of Project

--- src/studentinformationlist/controller/MainController.java
+++ src/studentinformationlist/controller/MainController.java
@@ -6,17 +6,30 @@
 package studentinformationlist.controller;
 
 import java.net.URL;
+import java.sql.Date;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import studentinformationlist.dao.StudentDAO;
+import studentinformationlist.model.Student;
 
 /**
  *
@@ -45,19 +58,114 @@ public class MainController implements Initializable {
     private Button saveButton;
     @FXML
     private DatePicker dobPicker;
+    @FXML
+    private TableView<Student> tableId;
     
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
-    
+    StudentDAO stdao;
+    @FXML
+    private TableColumn<Student, Integer> idCol;
+    @FXML
+    private TableColumn<Student, String> nameCol;
+    @FXML
+    private TableColumn<Student, String> emailCol;
+    @FXML
+    private TableColumn<Student, String> genderCol;
+    @FXML
+    private TableColumn<Student, Date> dobCol;
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        maleRadioBtn.setUserData("Male");
+        femaleRadioBtn.setUserData("Female");
+        stdao=new StudentDAO();
+        
+        initColumn();
+        loadTableData();
     }    
 
     @FXML
     private void saveStudentInfo(ActionEvent event) {
+        String name=nameField.getText();
+        String email=emailField.getText();
+        RadioButton selectedradio=(RadioButton)gender.getSelectedToggle();
+        String genderstr=(String)selectedradio.getUserData();
+        LocalDate localdob=dobPicker.getValue();
+        
+        if( name==null || email==null || localdob==null)
+        {
+            Alert alert=new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setContentText("Please,fill out all fields");
+            alert.show();
+            clearForm();
+            return;
+        }
+        else if(!emailField.getText().matches("^(.+)@(\\S+)$"))
+        {
+            Alert alert=new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Invalid Email");
+            alert.setContentText("Enter a valid Email");
+            alert.show();
+            emailField.clear();
+            return;
+        }
+        Date dob=Date.valueOf(localdob);
+        
+        Student student=new Student(name,email,genderstr,dob);
+        try {
+            stdao.saveStudents(student);
+            Alert alert=new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Success");
+            alert.setContentText("Student's Information is successful saved");
+            alert.show();
+            clearForm();
+            loadTableData();
+        } catch (SQLException ex) {
+            System.out.println("Error");
+       }
+    }
+
+    @FXML
+    private void closeWindow(ActionEvent event) {
+        Stage stage=(Stage)nameField.getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    private void deleteTableRow(ActionEvent event) throws SQLException {
+        int id=tableId.getSelectionModel().getSelectedItem().getId();
+        stdao.DeleteStudent(id);
+        loadTableData();
+    }
+
+    @FXML
+    private void goAboutProject(ActionEvent event) {
     }
     
+    @FXML
+    private void clearForm()
+    {
+        nameField.clear();
+        emailField.clear();
+        maleRadioBtn.setSelected(true);
+        dobPicker.setValue(null);
+    }
+    @FXML
+    private void initColumn()
+    {
+        idCol.setCellValueFactory(new PropertyValueFactory<>("id"));
+        nameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
+        emailCol.setCellValueFactory(new PropertyValueFactory<>("email"));
+        genderCol.setCellValueFactory(new PropertyValueFactory<>("gender"));
+        dobCol.setCellValueFactory(new PropertyValueFactory<>("dob"));
+    }
+    @FXML
+    private  void loadTableData()
+    {
+        try {
+            List<Student> studentlist=stdao.getStudents();
+            tableId.getItems().setAll(studentlist);
+        } catch (SQLException ex) {
+            System.out.println("Loading Error");
+        }
+    }
 }
